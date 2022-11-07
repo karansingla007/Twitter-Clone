6767be61e830f2108aab313ef59c8af1a58286aa
saurabhs679 <saurabhs679@gmail.com>
2022-11-07 18:03:40 -0500

    Complete testing of get tweets liked user list

--- test_api_base/end_points/test_tweet_api.py
+++ test_api_base/end_points/test_tweet_api.py
@@ -16,6 +16,13 @@ class TestTweetApi(unittest.TestCase, TestApiBase):
         self.assertIn("tweet_id", output, 'get tweet from user api 1 testcase failed')
         self.assertIn("description", output, 'get tweet from user api 2 testcase failed')
 
+    def test_get_tweets_liked_user_list(self):
+        super().use_mock_database()
+        output = TweetApi().handleGetTweetQuery(f'tweet/like/list?tweet_id=5', None)
+
+        self.assertIn("2811595370", output, 'get tweet from user api 1 testcase failed')
+        self.assertIn("606066960", output, 'get tweet from user api 2 testcase failed')
+
 
 if __name__ == '__main__':
     unittest.main()
