9223cd157c6211ba1dff44d177ec747ffbbefc35
saurabhs679 <saurabhs679@gmail.com>
2022-11-08 21:22:42 -0500

    Function reformatted and added two testcases.

--- test_api_base/end_points/test_feed_api.py
+++ test_api_base/end_points/test_feed_api.py
@@ -8,12 +8,12 @@ from test_api_base.test_api_base import ADMIN_USER_NAME, TestApiBase
 
 
 class TestFeedApi(unittest.TestCase, TestApiBase):
-    def test_get_tweets_from_user(self):
+    def test_get_user_feed(self):
         super().use_mock_database()
         output = FeedApi().handleGetFeedQuery(f'/feed/by/userName?user_name="{ADMIN_USER_NAME}"', None)
 
-        self.assertIn("tweet_id", output, 'get feed from user api 1 testcase failed')
-        self.assertIn("description", output, 'get feed from user api 2 testcase failed')
+        self.assertIn('''"tweet_id": 1''', output, 'get feed from user api 1 testcase failed')
+        self.assertIn('''"tweet_id": 3''', output, 'get feed from user api 2 testcase failed')
 
 
 if __name__ == '__main__':
