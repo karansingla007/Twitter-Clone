e2843b370375c9915a81f6a97a0821fde690ed28
Kara Singla <karansingla120@gmail.com>
2022-11-12 17:00:49 -0500

    Refactor code and add comments

--- api_base/end_point_handler/users_api.py
+++ api_base/end_point_handler/users_api.py
@@ -9,30 +9,38 @@ from twitter_api.api_client import fetchTwitterUserDetailByUserName
 class UsersApi(ApiBase):
     __sqliteHelperObj = SqliteHelper.instance()
 
-    # get all users list
     def __getAllUsersDetails(self):
+        """
+        Get Api:- get all users list
+        """
         selectUserDetailQuery = '''Select user_id, name, user_name, bio from user'''
         response = self.__sqliteHelperObj.execute_select_query(selectUserDetailQuery)
 
         return response
 
-    # get particular userId
     def __getUsersDetails(self, parsed):
+        """
+        Get Api:- get particular userId
+        """
         selectUserDetailQuery = f'''Select user_id, name, user_name, bio from user where user_id = {parsed['user_id'][0]}'''
         response = self.__sqliteHelperObj.execute_select_query(selectUserDetailQuery)
 
         return response
 
-    # get user followers list
     def __getUserFollowerList(self, parsed):
+        """
+        Get Api:- get user followers list
+        """
         selectUserDetailQuery = f'''Select user.name, user.user_name, user.bio from user Inner Join userFollower on 
         user.user_id = userFollower.follower_id where userFollower.user_id = {parsed['user_id'][0]} '''
         response = self.__sqliteHelperObj.execute_select_query(selectUserDetailQuery)
 
         return response
 
-    # get user following list
     def __getUserFollowingList(self, parsed):
+        """
+        Get Api:- get user following list
+        """
         selectUserDetailQuery = f'''Select user.name, user.user_name, user.bio from user Inner Join userFollowing on 
         user.user_id = userFollowing.following_id where userFollowing.user_id = {parsed['user_id'][0]} '''
         response = self.__sqliteHelperObj.execute_select_query(selectUserDetailQuery)
@@ -54,8 +62,10 @@ class UsersApi(ApiBase):
         response = {'response': 200}
         return response
 
-    # follow any user
     def __userFollow(self, parsed):
+        """
+        Post Api:- follow any user
+        """
         insertUserFollowerQuery = f'''Insert into userFollower(user_id, follower_id) values ({parsed['following_id'][0]}, {parsed['user_id'][0]})'''
         insertUserFollowingQuery = f'''Insert into userFollowing(user_id, following_id) values ({parsed['user_id'][0]}, {parsed['following_id'][0]})'''
 
@@ -65,8 +75,10 @@ class UsersApi(ApiBase):
         response = {'response': 200}
         return response
 
-    # unfollow any user
     def __userUnfollow(self, parsed):
+        """
+        Post Api:- unfollow any user
+        """
         deleteUserFollowerQuery = f'''Delete from userFollower where user_id = {parsed['following_id'][0]} AND 
         follower_id = {parsed['user_id'][0]}'''
         deleteUserFollowingQuery = f'''Delete from userFollowing where user_id = {parsed['user_id'][0]} AND following_id = {parsed['following_id'][0]}'''
@@ -76,9 +88,11 @@ class UsersApi(ApiBase):
         response = {'response': 200}
         return response
 
-    # Perform get operations on user
 
     def handleGetUserQuery(self, path, server):
+        """
+        Perform get operations on user
+        """
         parsed = super().parse_query_params(path)
         response = {}
         if path == '/user/all/details':
@@ -95,8 +109,10 @@ class UsersApi(ApiBase):
         else:
             super().return_success_response(response, server)
 
-    # Perform create, update, delete operations on user
     def handlePostUserQuery(self, path, server):
+        """
+        Perform create, update, delete operations on user
+        """
         parsed = super().parse_query_params(path)
         response = {}
         if '/user/follow' in path:
